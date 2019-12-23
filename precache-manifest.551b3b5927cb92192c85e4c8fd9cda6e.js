deb14ec002baf0ca9f24b90b64bf42534dec6940
abhishek singh <abhishek.singh@instarem.com>
2019-12-23 19:35:50 +0530

    Updates

---
+++ precache-manifest.551b3b5927cb92192c85e4c8fd9cda6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c888287ab1d5883088fa0f77f1b20cf2",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "f2a7de6cbddc0c62c4ab",
+    "url": "/movie/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e42ae300c1c093c99ee",
+    "url": "/movie/static/js/2.a5bd52dc.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/movie/static/js/2.a5bd52dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2a7de6cbddc0c62c4ab",
+    "url": "/movie/static/js/main.285058ab.chunk.js"
+  },
+  {
+    "revision": "c1223cfe94a4980d9472",
+    "url": "/movie/static/js/runtime-main.dee2beef.js"
+  }
+]);
